ff4786df79bba20a549d104cccdf28cfeb5c4166
qingziguanjun <809825586@qq.com>
2017-12-03 08:46:47 +0800

    虚拟机简单参数

---
+++ src/main/java/vitualmachine/chapter2/SimpleArgs.java
@@ -0,0 +1,12 @@
+package vitualmachine.chapter2;
+
+
+public class SimpleArgs {
+	public static void main(String[] args) {
+		for(int i = 0; i < args.length; i++){
+			System.out.println("参数"+(i+1) + ":" + args[i]);
+		}
+		System.out.println("Xmx" + Runtime.getRuntime().maxMemory()/1000/1000 + "M");
+		
+	}
+}
---
+++ src/main/java/vitualmachine/chapter2/SimpleHeap.java
@@ -0,0 +1,19 @@
+package vitualmachine.chapter2;
+
+public class SimpleHeap {
+	public int id;
+	public SimpleHeap(int id){
+		this.id = id;
+	}
+	
+	public void show() {
+		System.out.println("My ID is " + id);
+	}
+	
+	public static void main(String[] args) {
+		SimpleHeap s1 = new SimpleHeap(1);
+		SimpleHeap s2 = new SimpleHeap(2);
+		s1.show();
+		s2.show();
+	}
+}
